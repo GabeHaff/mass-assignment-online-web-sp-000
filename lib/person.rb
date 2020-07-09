dadcc07dabac03934b72f4327e465e40569d2492
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2020-07-09 19:06:57 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  #your code here
-end
+  
+  attr_accessor :name, :eye_color
+  
+  #@@attributes = []
+  
+  
+  def initialize(attributes={}) #(:name, :eye_color)
+   # @name = name 
+   # @eye_color = eye_color 
+   # @@attributes << name, eye_color
+    #
+    attributes.each{|key,value|self.send(("#{key}"),value)}
+  end 
+end #class end 
